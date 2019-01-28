f591517a9b6cc7f5cf06038269f974e565b19855
Supun Kandambige <supunkandambige9@gmail.com>
2019-01-28 10:16:32 +0530

    Updated demo.sh

--- demo.sh
+++ demo.sh
@@ -1,13 +1,16 @@
 file=$1
+root_path='/mnt/data/video-tagger-backend'
 feat='/mnt/data/video-tagger-backend/features'
-cd /home/supunK/GIT/c3d-keras
-source activate c3d
-python demo.py -p /mnt/data/video-tagger-backend/uploads/$file -v $file -f $feat
+vid_path='/mnt/data/video-tagger-backend/uploads'
 
-cd /home/supunK/GIT/DenseVideoCaptioning
+cd /home/supunK/FYP/c3d-keras
+source activate c3d
+python demo.py -p $vid_path/$file -v $file -f $feat
+
+cd /home/supunK/FYP/DenseVideoCaptioning
 source activate dense
-python demo.py -v $file -p /mnt/data/video-tagger-backend/uploads -f $feat 
+python demo.py -v $file -p $vid_path -f $feat 
 
 source deactivate
-cd /mnt/data/video-tagger-backend
+cd $root_path
 echo 'Done processing $file'
